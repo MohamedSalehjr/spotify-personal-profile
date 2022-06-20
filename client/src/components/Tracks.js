8969241036365857bccf2d681b9378e5fbb2dcff
Mohamed  Saleh <mohamedsalehjr@gmail.com>
2022-06-20 11:30:28 +0100

    typo update

--- client/src/components/Tracks.js
+++ client/src/components/Tracks.js
@@ -4,7 +4,7 @@ function Tracks (props){
 
     return(
         <div className="mt-8 flex flex-col">
-            <h2 className="ml-8 text-xl font-bold">Top Artists This Month</h2>
+            <h2 className="ml-8 text-xl font-bold">Top Tracks This Month</h2>
             <div className="mt-8 px-12 card-list grid grid-cols-1 sm:grid-cols-2 gap-4">
        {props.tracks && props.tracks.items.length ? props.tracks.items.map((track, i) => (
             <div className="flex flex-row p-4 items-center bg-green-500 rounded" key={i}>
